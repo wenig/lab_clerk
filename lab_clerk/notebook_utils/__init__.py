738b5be5d018368da40f008eb26d1f8b433d2c9d
wenig <info@pwenig.de>
2020-04-22 18:03:12 +0200

    version 0.0.2

--- lab_clerk/notebook_utils/__init__.py
+++ lab_clerk/notebook_utils/__init__.py
@@ -1,32 +1,46 @@
 from pandas import DataFrame
-from ipywidgets import SelectMultiple
+from ipywidgets import SelectMultiple, Select
 import matplotlib.pyplot as plt
 from typing import Callable
+import numpy as np
 
 
-def plot_tool(experiments: DataFrame, evaluations: DataFrame) -> Callable[[SelectMultiple, SelectMultiple, SelectMultiple], None]:
+def plot_tool(experiments: DataFrame, evaluations: DataFrame) -> Callable[[SelectMultiple, Select, SelectMultiple], None]:
     select_experiment_ids = SelectMultiple(
         options=experiments.id.values,
         value=[],
+        # rows=10,
         description='Experiments',
         disabled=False)
 
-    select_x = SelectMultiple(
+    select_x = Select(
         options=evaluations.columns,
-        value=[],
+        value=None,
         description='X Axis',
         disabled=False)
 
     select_y = SelectMultiple(
-        options=evaluations.columns,
+        options=np.unique(evaluations.metrics.values.sum()),
         value=[],
         description='Y Axis',
         disabled=False)
 
-    def plot(experiment_id: SelectMultiple = select_experiment_ids,
-             x_axis: SelectMultiple = select_x,
-             y_axis: SelectMultiple = select_y):
-        data = evaluations[evaluations.experiment_id.isin(experiment_id)]
-        plt.plot(data[list(x_axis)], data[list(y_axis)].apply(lambda x: x.map(lambda y: y[0])))
+    def plot(experiment_id=select_experiment_ids, x_axis=select_x, y_axis=select_y):
+        if len(experiment_id) > 0:
+            data = evaluations[evaluations.experiment_id.isin(experiment_id)]
+        else:
+            data = evaluations
+        subplots = []
+        n_metrics = len(y_axis) * 100
+        for i, metric in enumerate(y_axis):
+            if len(subplots) == 0:
+                ax = plt.subplot(n_metrics + 11 + i)
+            else:
+                ax = plt.subplot(n_metrics + 11 + i, sharex=subplots[0])
+            subplots.append(ax)
+            ind = data.metrics.iloc[0].index(metric)
+            plt.ylim(0.5, 1.0)
+            plt.plot(data[x_axis], data.vals.apply(lambda y: y[ind]))
+        plt.show()
 
     return plot
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lab_clerk",
-    version="0.0.1",
+    version="0.0.2",
     description="Experiment Tracking and Evaluation",
     author="Phillip Wenig",
     author_email="phillip.wenig@hpi.de",
@@ -12,6 +12,7 @@ setup(
     test_suite="tests",
     install_requires=[
         'pandas',
+        'numpy'
         'ipywidgets',
         'matplotlib'
     ]
